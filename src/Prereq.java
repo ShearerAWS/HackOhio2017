4f3b6b18eeb49eb4eb42e7de3186f49ac4b47d32
legobaseball2 <zacharywm3@gmail.com>
2017-10-21 12:09:39 -0400

    Prereq with Getters and Setters

--- src/Prereq.java
+++ src/Prereq.java
@@ -7,4 +7,35 @@ public class Prereq {
         this.course = c;
         this.Category = cat;
     }
+
+    /**
+     * @return the category
+     */
+    public char getCategory() {
+        return this.Category;
+    }
+
+    /**
+     * @param category
+     *            the category to set
+     */
+    public void setCategory(char category) {
+        this.Category = category;
+    }
+
+    /**
+     * @return the course
+     */
+    public Course getCourse() {
+        return this.course;
+    }
+
+    /**
+     * @param course
+     *            the course to set
+     */
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
 }
