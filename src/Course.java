e695183fbe0cadb2c729a95c2067426e83a76527
Jeff Asa-Hauser <32982293+jrasahauser@users.noreply.github.com>
2017-10-21 12:03:30 -0400

    Course Class

--- src/Course.java
+++ src/Course.java
@@ -2,24 +2,87 @@ import java.util.ArrayList;
 
 public class Course {
 
-    String courseID;
+    String Department;
+    int CourseNumber;
+    int SubNumber;
+    boolean IsHonors;
+
     //Strings in prereq include tag letter at beginning for parsing
-    ArrayList<String> prereq = new ArrayList<String>();
+    ArrayList<Prereq> requirements = new ArrayList<Prereq>();
 
-    public String getCourseID() {
-        return this.courseID;
+    /**
+     * @return the department
+     */
+    public String getDepartment() {
+        return this.Department;
     }
 
-    public void setCourseID(String courseID) {
-        this.courseID = courseID;
+    /**
+     * @param department
+     *            the department to set
+     */
+    public void setDepartment(String department) {
+        this.Department = department;
     }
 
-    public ArrayList<String> getPrereq() {
-        return this.prereq;
+    /**
+     * @return the courseNumber
+     */
+    public int getCourseNumber() {
+        return this.CourseNumber;
     }
 
-    public void setPrereq(ArrayList<String> prereq) {
-        this.prereq = prereq;
+    /**
+     * @param courseNumber
+     *            the courseNumber to set
+     */
+    public void setCourseNumber(int courseNumber) {
+        this.CourseNumber = courseNumber;
+    }
+
+    /**
+     * @return the subNumber
+     */
+    public int getSubNumber() {
+        return this.SubNumber;
+    }
+
+    /**
+     * @param subNumber
+     *            the subNumber to set
+     */
+    public void setSubNumber(int subNumber) {
+        this.SubNumber = subNumber;
+    }
+
+    /**
+     * @return the isHonors
+     */
+    public boolean isIsHonors() {
+        return this.IsHonors;
+    }
+
+    /**
+     * @param isHonors
+     *            the isHonors to set
+     */
+    public void setIsHonors(boolean isHonors) {
+        this.IsHonors = isHonors;
+    }
+
+    /**
+     * @return the requirements
+     */
+    public ArrayList<Prereq> getRequirements() {
+        return this.requirements;
+    }
+
+    /**
+     * @param requirements
+     *            the requirements to set
+     */
+    public void setRequirements(ArrayList<Prereq> requirements) {
+        this.requirements = requirements;
     }
 
 }
